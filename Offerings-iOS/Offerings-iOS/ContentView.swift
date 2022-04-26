980205b212f212f0f28508f97f1c0cc0d82a02e3
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2022-04-25 21:22:18 -0600

    initial commit

---
+++ Offerings-iOS/Offerings-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Offerings-iOS
+//
+//  Created by Dan Orton on 4/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Offerings-iOS/Offerings-iOS/Offerings_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Offerings_iOSApp.swift
+//  Offerings-iOS
+//
+//  Created by Dan Orton on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Offerings_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
